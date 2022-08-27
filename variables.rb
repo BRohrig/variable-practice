4f51e0b5723611e6c967091989fba1aae5adf225
Brady Rohrig <brady.rohrig@gmail.com>
2022-08-27 15:16:28 -0700

    Added puts

--- variables.rb
+++ variables.rb
@@ -6,3 +6,17 @@ height = 5.75
 height2 = 6.3
 hot = true
 married = true
+name # "mediha sorma"
+name2 # "brady rohrig"
+
+puts name
+puts name2
+
+puts age
+puts age2
+
+puts height
+puts height2
+
+puts hot
+puts married
